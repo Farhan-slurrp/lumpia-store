2cecc00e7160eae5584266ee5a31a487b49d8071
Farhan-slurrp <farhanlmntrix@gmail.com>
2021-07-30 15:07:35 +0800

    add meta tag for viweport

--- pages/index.js
+++ pages/index.js
@@ -8,45 +8,50 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="w-screen h-full bg-gray-300">
-      <Navbar />
-      <div
-        id="banner"
-        className="flex flex-col items-center justify-center w-full h-full px-5 pt-3 pb-5"
-      >
-        <Banner />
-        <h1
-          className="pt-12 mt-12 text-2xl font-bold font-merriweather"
-          id="menu"
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className="w-full bg-gray-300">
+        <Navbar />
+        <div
+          id="banner"
+          className="flex flex-col items-center justify-center w-full h-full px-5 pt-3 pb-5"
         >
-          - MENU -
-        </h1>
-        <Menus />
-        <h1
-          className="pt-12 mt-12 text-2xl font-bold font-merriweather"
-          id="galeri"
-        >
-          - GALERI -
-        </h1>
-        <Gallery />
-        <h1
-          className="pt-12 mt-12 text-2xl font-bold font-merriweather"
-          id="lokasi"
-        >
-          - LOKASI -
-        </h1>
-        <div className="flex items-center justify-center p-2 my-12 bg-white rounded-md">
-          <iframe
-            width="850"
-            height="450"
-            loading="lazy"
-            allowfullscreen
-            src={`https://www.google.com/maps/embed/v1/place?key=${process.env.MY_API_KEY}
+          <Banner />
+          <h1
+            className="pt-12 mt-12 text-2xl font-bold font-merriweather"
+            id="menu"
+          >
+            - MENU -
+          </h1>
+          <Menus />
+          <h1
+            className="pt-12 mt-12 text-2xl font-bold font-merriweather"
+            id="galeri"
+          >
+            - GALERI -
+          </h1>
+          <Gallery />
+          <h1
+            className="pt-12 mt-12 text-2xl font-bold font-merriweather"
+            id="lokasi"
+          >
+            - LOKASI -
+          </h1>
+          <div className="flex items-center justify-center p-2 my-12 bg-white rounded-md">
+            <iframe
+              width="850"
+              height="450"
+              loading="lazy"
+              allowfullscreen
+              src={`https://www.google.com/maps/embed/v1/place?key=${process.env.MY_API_KEY}
     &q=Kedai+lumpia+basah+ceuceu`}
-          ></iframe>
+            ></iframe>
+          </div>
+          <MoreInfo />
         </div>
-        <MoreInfo />
       </div>
-    </div>
+    </>
   );
 }
